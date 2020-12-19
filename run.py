0a8a9ee1b3651439c1908270d0a467d66e192507
delta114514 <gey3933@gmail.com>
2020-12-19 13:32:07 +0900

    create db dir

--- run.py
+++ run.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     if DEBUG:
         for file in glob.glob('static/db/*'):
             os.remove(file)
-
+    os.makedirs('static/db', exist_ok=True)
     uvicorn.run(app=app, host="0.0.0.0")
