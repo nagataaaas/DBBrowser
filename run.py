7558205e21962d031bae620bd59c76920b64a46e
delta114514 <gey3933@gmail.com>
2022-03-16 22:16:25 +0900

    removed files

--- run.py
+++ run.py
@@ -7,8 +7,4 @@ from config import DEBUG
 from urls import app
 
 if __name__ == '__main__':
-    if DEBUG:
-        for file in glob.glob('static/db/*'):
-            os.remove(file)
-    os.makedirs('static/db', exist_ok=True)
     uvicorn.run(app=app, host="0.0.0.0")
